362d5bbb2ab2ffd26a3ac515faa171a97dc4eb9d
Varun <varun.gandhi@allenoverseas.com>
2024-07-22 23:58:40 +0530

    initial commit

---
+++ src/pkgsample/__init__.py
@@ -0,0 +1,6 @@
+# The root of the entire project
+
+# This is the version number of your code.  This will change each time you make
+# a new release.  The most popular approach is to use Semantic Versioning,
+# described at https://semver.org/
+__version__ = "0.1.1"
